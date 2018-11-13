d3d3cbacd3bd35a8939748e4e40236aa8d15f7d6
ErinaceusLinnaeus <benji@fstop8.de>
2018-11-13 13:30:51 +0100

    Update src/Kontrol/Kontrol.ino
    Describing the project...

---
+++ src/Kontrol/Kontrol.ino
@@ -0,0 +1,16 @@
+/*(C)2018 Benjamin Schmid
+ * 
+ * Kontrol is an Arduino Project to control KSP.
+ * The hardware includes two 3-axis-joysticks and a 7" touchscreen.
+ * Interfacing with KSP through KerbalSimPit, an Arduino library written by Peter Hardy.
+ */
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
