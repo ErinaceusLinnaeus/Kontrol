607b1ede516cb0af7f29785ed12485a037014571
ErinaceusLinnaeus <benji@fstop8.de>
2019-01-28 15:40:17 +0100

    Update Kontrol.ino
    No keyboard planned anymore

--- src/Kontrol/Kontrol.ino
+++ src/Kontrol/Kontrol.ino
@@ -1,9 +1,9 @@
 /* (C)2019 Benjamin Schmid
-
-  Kontrol is an Arduino Project to control KSP.
-  The hardware includes two 3-axis-joysticks, a keyboard, a 12" HDMI Display, a 10" resistiv touchpanel.
-  Interfacing with KSP through KerbalSimPit, an Arduino library written by Peter Hardy.
-*/
+ *  
+ *  Kontrol is an Arduino Project to control KSP.
+ *  The hardware includes two 3-axis-joysticks, a 12" HDMI Display, a 10" resistiv touchpanel.
+ *  Interfacing with KSP through KerbalSimPit, an Arduino library written by Peter Hardy.
+ */
 
 /*
   Things that do work:
