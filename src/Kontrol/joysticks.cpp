4a43feff69f8ad5af669baf6fca8fefa803fd6fd
ErinaceusLinnaeus <benji@fstop8.de>
2019-01-28 12:45:32 +0100

    Update joysticks.cpp
    simpler naming of variables
    possibility to set different precisions for each axis

--- src/Kontrol/joysticks.cpp
+++ src/Kontrol/joysticks.cpp
@@ -9,73 +9,81 @@ int buttonRCSunprecise = 20;
 //It's not that bad. 327.67 is 1%
 //A variable deadband would be wonderful
 //I'll think about that
-int joysticksDeadbandPitch = 900;
-int joysticksDeadbandYaw = 1700; //~5.5%
-int joysticksDeadbandRoll = 650;
+int DeadbandPitch = 900;
+int DeadbandYaw = 1700; //~5.5%
+int DeadbandRoll = 650;
 
-int joysticksDeadbandX = 1000;
-int joysticksDeadbandY = 1000;
-int joysticksDeadbandZ = 650;
+int DeadbandX = 1000;
+int DeadbandY = 1000;
+int DeadbandZ = 650;
 
-int potiSASpitch = A0;
-int potiSASyaw = A1;
-int potiSASroll = A2;
+int potiPitch = A0;
+int potiYaw = A1;
+int potiRoll = A2;
 
-int potiRCSx = A3;
-int potiRCSy = A4;
-int potiRCSz = A5;
+int potiX = A3;
+int potiY = A4;
+int potiZ = A5;
+
+int precisionPitch = 3;
+int precisionYaw = 3;
+int precisionRoll = 10;
+
+int precisionX = 10;
+int precisionY = 10;
+int precisionZ = 10;
 
 void initializePins() {
   
   pinMode(buttonSASunprecise, INPUT_PULLUP);
   pinMode(buttonRCSunprecise, INPUT_PULLUP);
   
-  pinMode(potiSASpitch, INPUT);
-  pinMode(potiSASyaw, INPUT);
-  pinMode(potiSASroll, INPUT);
+  pinMode(potiPitch, INPUT);
+  pinMode(potiYaw, INPUT);
+  pinMode(potiRoll, INPUT);
   
-  pinMode(potiRCSx, INPUT);
-  pinMode(potiRCSy, INPUT);
-  pinMode(potiRCSz, INPUT);
+  pinMode(potiX, INPUT);
+  pinMode(potiY, INPUT);
+  pinMode(potiZ, INPUT);
 }
 
 //The range is supposed to be -32768, 32767 but it kept jumping to -32768
 int getSASpitch() {
   
-  int16_t SASpitch = map(analogRead(potiSASpitch), 0, 1023, -32700, 32700);
+  int16_t SASpitch = map(analogRead(potiPitch), 0, 1023, -32700, 32700);
   
-  if(abs(SASpitch) - joysticksDeadbandPitch < 0)
+  if(abs(SASpitch) - DeadbandPitch < 0)
     SASpitch = 0;
 
   //pitch's the wrong way, of course
   SASpitch = SASpitch * -1;
     
   if (!digitalRead(buttonSASunprecise))
-    return SASpitch / 10;
+    return SASpitch / 3;
   else
     return SASpitch;
 }
 
 int getSASyaw() {
   
-  int16_t SASyaw = map(analogRead(potiSASyaw), 0, 1023, -32700, 32700);
+  int16_t SASyaw = map(analogRead(potiYaw), 0, 1023, -32700, 32700);
     
-  if(abs(SASyaw) - joysticksDeadbandYaw < 0)
+  if(abs(SASyaw) - DeadbandYaw < 0)
     SASyaw = 0;
-    
+  
   if (!digitalRead(buttonSASunprecise))
-    return SASyaw / 10;
+    return SASyaw / 3;
   else
     return SASyaw;
 }
 
 int getSASroll() {
     
-  int16_t SASroll = map(analogRead(potiSASroll), 0, 1023, -32700, 32700);
+  int16_t SASroll = map(analogRead(potiRoll), 0, 1023, -32700, 32700);
   
-  if(abs(SASroll) - joysticksDeadbandRoll < 0)
+  if(abs(SASroll) - DeadbandRoll < 0)
     SASroll = 0;
-    
+  
   if (!digitalRead(buttonSASunprecise))
     return SASroll / 10;
   else
@@ -84,11 +92,11 @@ int getSASroll() {
 
 int getRCSx() {
   
-  int16_t RCSx = map(analogRead(potiRCSx), 0, 1023, -32700, 32700);
+  int16_t RCSx = map(analogRead(potiX), 0, 1023, -32700, 32700);
   
-  if(abs(RCSx) - joysticksDeadbandX < 0)
+  if(abs(RCSx) - DeadbandX < 0)
     RCSx = 0;
-
+  
   //x's the wrong way?
   RCSx = RCSx * -1;
     
@@ -101,11 +109,11 @@ int getRCSx() {
 
 int getRCSy() {
   
-  int16_t RCSy = map(analogRead(potiRCSy), 0, 1023, -32700, 32700);;
+  int16_t RCSy = map(analogRead(potiY), 0, 1023, -32700, 32700);;
   
-  if(abs(RCSy) - joysticksDeadbandY < 0)
+  if(abs(RCSy) - DeadbandY < 0)
     RCSy = 0;
-    
+  
   if (!digitalRead(buttonRCSunprecise))
     return RCSy / 10;
   else
@@ -115,9 +123,9 @@ int getRCSy() {
 
 int getRCSz() {
   
-  int16_t RCSz = map(analogRead(potiRCSz), 0, 1023, -32700, 32700);
+  int16_t RCSz = map(analogRead(potiZ), 0, 1023, -32700, 32700);
   
-  if(abs(RCSz) - joysticksDeadbandZ < 0)
+  if(abs(RCSz) - DeadbandZ < 0)
     RCSz = 0;
 
   //z's the wrong way, forward is negative?
